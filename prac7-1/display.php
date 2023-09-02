61c3e2569b76340cbbff3d94d33f06ff4f420ade
josssss07 <joshuadesai03@gmail.com>
2023-09-02 10:49:30 +0530

    init

--- prac7-1/display.php
+++ prac7-1/display.php
@@ -11,6 +11,49 @@
     </style>
 </head>
 <body>
-    
+        <h1>Data from Table</h1>
+
+    <?php
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "empdetailsDB";
+        $conn = mysqli_connect($hostname, $username, $password, $database);
+        if(!$conn){
+            die("Connection failed: ".mysqli_connect_error());
+        }
+
+        $sql ="SELECT * FROM emp_general_details";
+        $result = mysqli_query($conn , $sql);
+
+        if(mysqli_num_rows($result)>0){
+            echo"<table border = '1'>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</name>
+                        <th>Department</th>
+                        <th>Position</th>
+                    </tr>";
+
+                    while($row = mysqli_fetch_assoc($result)){
+                        echo"
+                            <tr>
+                                <td>".$row['emp_id']."</td>
+                                <td>".$row['emp_name']."</td>
+                                <td>".$row['department']."</td>
+                                <td>".$row['position']."</td>
+                            </tr>
+                        ";
+
+                    }
+                    echo"</table>";
+
+        }
+        else{
+            echo("NO DATA FOUND");
+        }
+
+    mysqli_close($conn);
+    ?>
 </body>
 </html>
