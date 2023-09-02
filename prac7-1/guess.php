736cda938a110eda95b633f7160c18d2c5eaa578
josssss07 <joshuadesai03@gmail.com>
2023-09-02 10:06:03 +0530

    guess.init based php

---
+++ prac7-1/guess.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guessing game</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h3>Word: </h3>
+                <label for="word_guessed">
+                    <input type="text" name="word_guessed" id="word_guessed">
+                </label>
+            </div>
+            <div class="col-sm-4">
+                
+            </div>
+            div
+        </div>  
+        <div class="row">
+            <div class="col-sm-4" id="word">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-2" id="check">
+                <button id="check">Check</button>
+                <button id="rest_check">Reset</button>
+                <button id="next-word">Next Word</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
